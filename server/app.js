25e41e57b3c8ac20dec3f8fe0e674e04e042f09a
zacharyhester2 <zachary.hester2@gmail.com>
2021-04-03 10:23:07 -0500

    created server on 3001

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+
+// const { Maps } = require('./api/googleMaps');
+
+const CLIENT_PATH = path.resolve(__dirname, '../client/dist');
+const app = express();
+
+app.use(express.static(CLIENT_PATH));
+
+//middleware
+// app.use(express.join());
+//set API FOLDER to api/ googleMaps (or change if named something else)
+// app.use('/api/googleMaps', Maps); 
+
+module.exports = {
+  app,
+};
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+});
+
+const { app } = require('./app');
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`http://127.0.0.1:${PORT}`);
+});
+
