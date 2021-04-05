035e5436ab7bc225075d7e5d10d955b503bb143a
Sarah St.Ores <sarahstores11@gmail.com>
2021-04-05 14:26:19 -0500

    google sign in button rendered

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,10 @@
+import { gapi } from 'gapi-script';
 import React, {Component} from 'react';
 import styled from 'styled-components';
 import Map from './Map.jsx';
 import Calendar from './Calendar.jsx';
 import UserProfile from './UserProfile.jsx';
-import { withScriptjs, withGoogleMap } from 'react-google-maps'; 
+import { withScriptjs, withGoogleMap } from 'react-google-maps';
 import { apiKey } from '../../../key.js';
 
 const AppStyles = styled.div`
@@ -12,13 +13,38 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      isSignedIn: null
     };
+    this.initializeGoogleSignin = this.initializeGoogleSignin.bind(this)
+  }
+
+  initializeGoogleSignin() {
+    gapi.load('auth2', () => {
+      gapi.auth2.init({
+        client_id: '636707500167-jl0be6d4pi4e96ttgqkvt1v0758a3r9p.apps.googleusercontent.com'
+      })
+      console.log('api init successful')
+
+      gapi.load('signin2', () => {
+        const params = {
+          onSuccess: () => {
+            console.log('User has finished signing in!')
+          }
+        }
+        gapi.signin2.render('loginButton', params)
+      })
+    })
+  }
+
+  componentDidMount() {
+    console.log('Loading')
+
+    this.initializeGoogleSignin();
   }
 
   render() {
-   
-    const WrappedMap = withScriptjs(withGoogleMap(Map)); 
+
+    const WrappedMap = withScriptjs(withGoogleMap(Map));
 
     return (
       <AppStyles>
@@ -27,19 +53,20 @@ class App extends Component {
             <h1>Bike Around and Find Out</h1>
             <h3>New Orleans Interactive Community Bike Map</h3>
           </header>
-        <div style={{width: '100vw', height: '100vh'}}>
-          <WrappedMap
-            googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${apiKey}`}
-            loadingElement={<div style={{ height: '100%' }} />}
-            containerElement={<div style={{ height: '100%' }} />}
-            mapElement={<div style={{ height: '100%' }} />}
-          />
-        </div>
+          <div id="loginButton">Sign in with Google</div>
+          <div style={{width: '100vw', height: '100vh'}}>
+            <WrappedMap
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${apiKey}`}
+              loadingElement={<div style={{ height: '50%' }} />}
+              containerElement={<div style={{ height: '100%' }} />}
+              mapElement={<div style={{ height: '100%' }} />}
+            />
+          </div>
         <Calendar />
         <UserProfile />
       </div>
       </AppStyles>
- 
+
     );
   }
 }
